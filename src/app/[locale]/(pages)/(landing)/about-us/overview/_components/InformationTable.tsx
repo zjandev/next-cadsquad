48f7aea928638ccebc861e391cbf172b8522599f
Duong Cao <caohaiduong04@gmail.com>
2025-06-25 18:54:44 +0700

    Updating OverviewPg

---
+++ src/app/[locale]/(pages)/(landing)/about-us/overview/_components/InformationTable.tsx
@@ -0,0 +1,119 @@
+import React from 'react'
+
+import { useLocale } from 'next-intl'
+
+import { SupportLanguages } from '@/i18n/routing'
+
+export default function InformationTable() {
+    const locale = useLocale()
+
+    const aboutCadsquadInfo = [
+        {
+            enTitle: 'Company name',
+            viTitle: 'Tên công ty',
+            enValue: 'CADSQUAD',
+            viValue: 'CADSQUAD',
+        },
+        {
+            enTitle: 'Address',
+            viTitle: 'Địa chỉ',
+            enValue: 'Ho Chi Minh City, Viet Nam',
+            viValue: 'TP. Hồ Chí Minh, Việt Nam',
+        },
+        {
+            enTitle: 'Phone number',
+            viTitle: 'Số điện thoại',
+            enValue: '(+84) 765 279 228',
+            viValue: '(+84) 765 279 228',
+        },
+        {
+            enTitle: 'Email',
+            viTitle: 'Email',
+            enValue: 'Contact@cadsquad.vn',
+            viValue: 'Contact@cadsquad.vn',
+        },
+        {
+            enTitle: 'Establishment',
+            viTitle: 'Năm thành lập',
+            enValue: '10/11/2022',
+            viValue: '10/11/2022',
+        },
+        {
+            enTitle: 'Founder',
+            viTitle: 'Nhà sáng lập',
+            enValue: 'Pham Tien Phong (Mr.)',
+            viValue: 'Phạm Tiền Phong',
+        },
+        {
+            enTitle: 'Business activities',
+            viTitle: 'Sản phẩm & dịch vụ',
+            enValues: [
+                'Technical Drawing Digitization & CAD Data Creation',
+                'Part Extraction from Assembly Drawings',
+                'Custom Machine Design for Food & Material Handling Systems',
+                '3D and 2D for Steel structure',
+                'CAD for Sheet Metal Manufacturing',
+                'Machine Design Support - From Concept to Manufacturing',
+                '3D Modeling & 3D Printing Support',
+                'Structural Analysis (FEA)',
+                'Mechanical Motion Simulation',
+                'Custom Design Services on Request',
+            ],
+            viValues: [
+                'Số hóa bản vẽ kỹ thuật & Tạo dữ liệu CAD',
+                'Tách chi tiết từ bản vẽ lắp ráp',
+                'Thiết kế máy tùy chỉnh cho hệ thống thực phẩm & xử lý vật liệu',
+                'Thiết kế 3D và 2D cho kết cấu thép',
+                'Thiết kế CAD cho gia công kim loại tấm',
+                'Hỗ trợ thiết kế máy – Từ ý tưởng đến sản xuất',
+                'Hỗ trợ mô hình 3D & In 3D',
+                'Phân tích kết cấu (FEA)',
+                'Mô phỏng chuyển động cơ khí',
+                'Dịch vụ thiết kế tùy chỉnh theo yêu cầu',
+            ],
+        },
+    ]
+    return (
+        <table className="w-full border border-gray-300 shadow-md">
+            {aboutCadsquadInfo.map((item, index) => {
+                const title = item[`${locale as SupportLanguages}Title`]
+
+                const value = item[`${locale as SupportLanguages}Value`]
+                const values = item[`${locale as SupportLanguages}Values`]
+
+                return (
+                    <tr
+                        key={item.enTitle + index}
+                        className="grid grid-cols-[0.5fr_1fr] gap-14 border-b border-gray-200 last:border-none"
+                    >
+                        <td className="uppercase font-semibold bg-gray-200 align-middle py-5 pl-10">
+                            {title}
+                        </td>
+                        {(item.enValue || item.viValue) && (
+                            <td className="align-middle py-5 tracking-wide">
+                                {value}
+                            </td>
+                        )}
+
+                        {(item.enValues || item.viValues) && (
+                            <td className="pl-8 py-5">
+                                <ul className="list-disc space-y-2">
+                                    {values?.map((item, index) => {
+                                        return (
+                                            <li
+                                                key={item + index}
+                                                className="tracking-wide"
+                                            >
+                                                {item}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </td>
+                        )}
+                    </tr>
+                )
+            })}
+        </table>
+    )
+}
---
+++ src/app/[locale]/(pages)/(landing)/about-us/overview/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Image from 'next/image'
+
+import ImgFounderBanner from '@/assets/images/founder-banner.png'
+
+import InformationTable from './_components/InformationTable'
+
+export default function OverviewPage() {
+    return (
+        <div className="pb-32 max-w-screen">
+            <section className="container space-y-9 mt-14">
+                <div className="flex flex-col items-center font-saira space-y-2">
+                    <h2 className="uppercase font-medium text-xl">Overview</h2>
+                    <h1 className="uppercase font-bold text-4xl">
+                        About CADSQUAD
+                    </h1>
+                </div>
+                <InformationTable />
+            </section>
+            <section className="mt-32">
+                <p>Câu chuyện người sáng lập</p>
+                <Image src={ImgFounderBanner} alt="Founder Banner Image" />
+            </section>
+        </div>
+    )
+}
